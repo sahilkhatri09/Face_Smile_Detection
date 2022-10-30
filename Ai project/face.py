93e5611a153a13ab705789d444df04013bb14921
sahilkhatri09 <88265012+sahilkhatri09@users.noreply.github.com>
2022-10-30 20:32:08 +0530

    Update face.py

--- Ai project/face.py
+++ Ai project/face.py
@@ -13,27 +13,25 @@ face_detector = cv2.CascadeClassifier('face_detector.xml')
 
 
 
-# # PLAYER 1
-# #LOADING IMAGE
-# image_path = 'test_images/vk1.jpg'
-# img = cv2.imread(image_path)
+ #1
+#LOADING IMAGE
+image_path = 'test_images/vk1.jpg'
+img = cv2.imread(image_path)
 
-# #CONVERTING TO GRAYSCALE
-# grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#CONVERTING TO GRAYSCALE
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# #DETECT FACES
-# face_coordinates = face_detector.detectMultiScale(grayscaled_img)
+#DETECT FACES
+face_coordinates = face_detector.detectMultiScale(grayscaled_img)
 
-# #DRAW GREEN RECTANGLE[S] AROUND THE PREDICTED FACE[S]
-# for face_coordinate in face_coordinates:
-#     (x, y, w, h)=face_coordinate
-#     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+#DRAW GREEN RECTANGLE[S] AROUND THE PREDICTED FACE[S]
+for face_coordinate in face_coordinates:
+    (x, y, w, h)=face_coordinate
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-# #SHOWING IMAGE
-# cv2.imshow('RDJ', img)
-# cv2.waitKey()
-
-
+#SHOWING IMAGE
+cv2.imshow('RDJ', img)
+cv2.waitKey()
 
 
 
@@ -42,26 +40,28 @@ face_detector = cv2.CascadeClassifier('face_detector.xml')
 
 
 
-# # PLAYER 2
-# #LOADING WEBCAME STREAM
-# webcam = cv2.VideoCapture(0)
-# #PASS VIDEO PATH AS AN ARGUMENT TO ABOVE FUNCTION TO DETECT FACES IN LOCAL VIDEOS
 
-# #SAME THING ON WEBCAM
-# while True:
-#     is_frame_read_success, frame = webcam.read()
-#     grayscaled_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     face_coordinates = trained_face_data.detectMultiScale(grayscaled_frame)
-#     for face_coordinate in face_coordinates:
-#         (x, y, w, h)=face_coordinate
-#         cv2.rectangle(frame, (x, y), (x+w, y+h), (randrange(255), randrange(255), randrange(255)), 2)
-#     cv2.imshow('WEBCAM', frame)
-#     key = cv2.waitKey(1)
 
-#     if(key==81 or key==113):
-#         break
+#  2
+#LOADING WEBCAME STREAM
+webcam = cv2.VideoCapture(0)
+#PASS VIDEO PATH AS AN ARGUMENT TO ABOVE FUNCTION TO DETECT FACES IN LOCAL VIDEOS
 
-# webcam.release()
+#SAME THING ON WEBCAM
+while True:
+    is_frame_read_success, frame = webcam.read()
+    grayscaled_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_frame)
+    for face_coordinate in face_coordinates:
+        (x, y, w, h)=face_coordinate
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (randrange(255), randrange(255), randrange(255)), 2)
+    cv2.imshow('WEBCAM', frame)
+    key = cv2.waitKey(1)
+
+    if(key==81 or key==113):
+        break
+
+webcam.release()
 
 
 
